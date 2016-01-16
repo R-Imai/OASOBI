46edf1fe3418e59add79c0e647ece0c50af33c6b
R-Imai <s1426015eq@s.chibakoudai.jp>
2016-01-16 17:36:06 +0900

    Reduction of the image for improve the processing speed

--- Sophomore/ImageProcessing/edgeDetection.cpp
+++ Sophomore/ImageProcessing/edgeDetection.cpp
@@ -2,7 +2,7 @@
 *	Name:		edgeDetection.cpp
 *	Author:		R.Imai
 *	Created:	2016 / 01 / 15
-*	Last Date:	2016 / 01 / 15
+*	Last Date:	2016 / 01 / 16
 *	Note:
 *
 *--------------------------------------------------------------------------------------------------------------*/
@@ -45,6 +45,7 @@ int main(int argc, char *argv[])
 {
 
 	int B, G, R;
+	int threshold = 80;
 	cv::Vec3b bgr;
 	cv::VideoCapture cap(0);
 	cv::Mat originImg;// = cv::imread("robo.jpg", 1);
@@ -52,31 +53,37 @@ int main(int argc, char *argv[])
 	while (cv::waitKey(1) != 'q'){
 		cap >> originImg;
 		if (originImg.empty()) continue;
+
+		double scale = 4.0;
+		cv::Mat smallImg(cv::saturate_cast<int>(originImg.rows / scale), cv::saturate_cast<int>(originImg.cols / scale), CV_8UC1);
+		// èàóùéûä‘íZèkÇÃÇΩÇﬂÇ…âÊëúÇèkè¨
+		cv::resize(originImg, smallImg, smallImg.size(), 0, 0, cv::INTER_LINEAR);
+
+		cv::Mat smallWhite(cv::Size(smallImg.cols, smallImg.rows), CV_8UC3, cv::Scalar::all(255));
 		cv::Mat whiteImg(cv::Size(originImg.cols, originImg.rows), CV_8UC3, cv::Scalar::all(255));
 		cv::Mat paletteImg(cv::Size(originImg.cols, originImg.rows), CV_8UC3, cv::Scalar::all(255));
-
-		for (int y = 1; y < originImg.rows - 1; y++) {
-			cv::Vec3b* ptr = whiteImg.ptr<cv::Vec3b>(y);
+		for (int y = 1; y < smallImg.rows - 1; y++) {
+			cv::Vec3b* ptr = smallWhite.ptr<cv::Vec3b>(y);
 			cv::Vec3b* pal = paletteImg.ptr<cv::Vec3b>(y);
-			for (int x = 1; x < originImg.cols - 1; x++) {
+			for (int x = 1; x < smallImg.cols - 1; x++) {
 				B = 0;
 				G = 0;
 				R = 0;
 				for (int i = 0; i < 3; i++){
 					for (int j = 0; j < 3; j++){
-						bgr = getBGR(originImg, x - 1 + i, y - 1 + j);
+						bgr = getBGR(smallImg, x - 1 + i, y - 1 + j);
 						B += bgr[0] * mask[i][j];
 						G += bgr[1] * mask[i][j];
 						R += bgr[2] * mask[i][j];
 					}
 				}
-				if (B > 100){
+				if (B > threshold){
 					B = 255;
 				}
-				if (G > 100){
+				if (G > threshold){
 					G = 255;
 				}
-				if (R > 100){
+				if (R > threshold){
 					R = 255;
 				}
 				if (B != 255 || G != 255 || R != 255){
@@ -85,14 +92,15 @@ int main(int argc, char *argv[])
 					R = 0;
 				}
 				ptr[x] = cv::Vec3b(B, G, R);
-				pal[x] = cv::Vec3b(getBGR(originImg, x, y)[0], 0, 0);
+				pal[x] = cv::Vec3b(getBGR(smallImg, x, y)[0], 0, 0);
 			}
 		}
+
+		cv::resize(smallWhite, whiteImg, whiteImg.size(), 0, 0, cv::INTER_LINEAR);
 
 		cv::imshow("nomal image", originImg);
 		cv::imshow("edging image", whiteImg);
 		//cv::imshow("palette image", paletteImg);
 		cvSetMouseCallback("nomal image", Mouse);
 	}
-	cv::waitKey(0);
 }
