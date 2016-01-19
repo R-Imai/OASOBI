1b8b505753b7ed281658dea9fc0ac47a940cfc00
R-Imai <s1426015eq@s.chibakoudai.jp>
2016-01-19 23:55:47 +0900

    add prewitt()

--- Sophomore/ImageProcessing/edgeDetection.cpp
+++ Sophomore/ImageProcessing/edgeDetection.cpp
@@ -20,7 +20,7 @@ int X[450], Y[450];
 int cnt = 0;
 int label[450][450];
 int count = 1;
-int mask[3][3] = { { -1, -1, -1 }, { -1, 8, -1 }, { -1, -1, -1 } };
+int mask[3][3] = { { 1, 1, 1 }, { 1, -2, 1 }, { -1, -1, -1 } };
 
 void Mouse(int event, int x, int y, int flags, void *param = NULL)
 {
@@ -32,6 +32,33 @@ void Mouse(int event, int x, int y, int flags, void *param = NULL)
 
 
 	}
+}
+
+void prewitt(int ang, int mag = 1){
+	int base[8] = { 1, 1, 1, 1, 1, -1, -1, -1 };
+	int slide[8] = {};
+	int n;
+	ang = ang / 45;
+	for (int i = 0; i < 8; i++){
+		n = ang + i;
+		if (n > 7){
+			n = n - 8;
+		}
+		slide[n] = base[i];
+	}
+	mask[0][0] = slide[1] * mag;
+	mask[1][0] = slide[2] * mag;
+	mask[2][0] = slide[3] * mag;
+	mask[0][1] = slide[0] * mag;
+	mask[1][1] = -2 * mag;
+	mask[2][1] = slide[4] * mag;
+	mask[0][2] = slide[7] * mag;
+	mask[1][2] = slide[6] * mag;
+	mask[2][2] = slide[5] * mag;
+	cout << mask[0][0] << "\t" << mask[1][0] << "\t" << mask[2][0] << "\t" << "\n";
+	cout << mask[0][1] << "\t" << mask[1][1] << "\t" << mask[2][1] << "\t" << "\n";
+	cout << mask[0][2] << "\t" << mask[1][2] << "\t" << mask[2][2] << "\t" << "\n";
+
 }
 
 
@@ -49,7 +76,7 @@ int main(int argc, char *argv[])
 	cv::Vec3b bgr;
 	cv::VideoCapture cap(0);
 	cv::Mat originImg;// = cv::imread("robo.jpg", 1);
-
+	prewitt(45);
 	while (cv::waitKey(1) != 'q'){
 		cap >> originImg;
 		if (originImg.empty()) continue;
